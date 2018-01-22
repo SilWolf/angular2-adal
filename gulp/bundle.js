9f211ae10b67eb0bdcec27c672906ddaf05b4b91
SilWolf <silwolf@hotmail.com.hk>
2018-01-22 15:16:29 +0800

    with angular 5

--- gulp/bundle.js
+++ gulp/bundle.js
@@ -21,15 +21,20 @@ const bundleConfig = {
     map: {
         '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
         '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
         '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
         '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-        'rxjs': 'npm:rxjs'
+        'rxjs': 'npm:rxjs',
+        'tslib': 'npm:tslib'
     },
     packages: {
 
         'rxjs': {
             main: 'Rx.js',
+            defaultExtension: 'js'
+        },
+        'tslib': {
+            main: 'tslib.js',
             defaultExtension: 'js'
         }
     }
